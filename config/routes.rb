f4f24426cc685777c3cc3fe7fe840eb38d83290e
Tish Lew <tish@change.org>
2013-12-30 17:49:34 -0800

    add bootstrap and refactor spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  root to: 'static_pages#home'
+  match "/help", to: "static_pages#help"
+  match "/about", to: "static_pages#about"
+  match "/contact", to: "static_pages#contact"
+  match "/signin", to: "static_pages#signin"
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -49,14 +48,4 @@ SampleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
